23cc452c611526c56b59afc32a0a871a93d86d97
Vágó Rebeka <rebeka.vago19@gmail.com>
2022-03-03 23:03:23 +0100

    második feladat, második feladatrész nem sikerült

--- feladat2.js
+++ feladat2.js
@@ -28,4 +28,14 @@ class Pont {
     toString(){
         return `Pont: x = ${this.#x}, y = ${this.#y}`;
     }
+    static tavolsag(p1, p2){
+        let x1 = p1.getX();
+        let y1 = p1.getY();
+        let y2 = p2.getY();
+        let x2 = p2.getX();
+        let x3 = abs(x1 - x2);
+        let y3 = abs(y1 - y2);
+        let kulonbseg = Math.sqrt((x3 ** 2) + (y3 ** 2));
+        console.log(kulonbseg);
+    }
 }
